3c5db7510f26e085189a4b47f2807730010f768b
Jigar <jigar@devrepublic.nl>
2022-03-31 09:52:10 +0530

    add qualification

--- app/Http/Controllers/QualificationController.php
+++ app/Http/Controllers/QualificationController.php
@@ -3,8 +3,87 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Qualification;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 class QualificationController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        try {
+            $selectArr = [
+                'id', 'name',
+                DB::raw('IF(is_active, "Active", "Inactive") AS status')
+            ];
+            return Qualification::select($selectArr)->cursorPaginate($request->limit ?? 5)->withQueryString();
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
+
+    public function show(Qualification $qualification)
+    {
+        try {
+            if ($qualification) {
+                return response()->json(['data' => $qualification, 'status' => true], 200);
+            } else {
+                return response()->json(['data' => null, 'status' => false], 404);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(),[
+                'name' => "required|unique:qualifications,name|max:125"
+            ]);
+            if ($validator->fails()) {
+                return response()->json(["message" => $validator->messages()], 201);
+            }
+
+            if (Qualification::create($request->all())) {
+                return response()->json(["message" => 'success', 'success' => true], 201);
+            } else {
+                return response()->json(["error" => true, 'message' => 'something_wrong'], 500);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
+
+    public function update(Request $request, Qualification $qualification)
+    {
+        try {
+            $validator = Validator::make($request->all(),[
+                'name' => "required|unique:qualifications,name,$qualification->id|max:125"
+            ]);
+            if ($validator->fails()) {
+                return response()->json(["message" => $validator->messages()], 201);
+            }
+
+            if ($qualification->update($request->all())) {
+                return response()->json(["message" => 'success', 'success' => true], 201);
+            } else {
+                return response()->json(["error" => true, 'message' => 'something_wrong'], 500);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
+
+    public function destory(Qualification $qualification)
+    {
+        try {
+            if ($qualification->delete()) {
+                return response()->json(['message' => 'success', 'status' => true], 200);
+            } else {
+                return response()->json(['message' => 'something_wrong', 'status' => true], 500);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,15 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+//--------------Branch---------------------------
 Route::get('branch',[App\Http\Controllers\BranchController::class,'index'])->name('branch.list');
 Route::get('branch/{branch}',[App\Http\Controllers\BranchController::class,'show'])->name('branch.show');
 Route::post('branch',[App\Http\Controllers\BranchController::class,'store'])->name('branch.save');
 Route::put('branch/{branch}',[App\Http\Controllers\BranchController::class,'update'])->name('branch.update');
 Route::delete('branch/{branch}',[App\Http\Controllers\BranchController::class,'destroy'])->name('branch.destroy');
-
+//--------------Course---------------------------
 Route::get('course',[App\Http\Controllers\CourseController::class,'index'])->name('course.list');
 Route::get('course/{course}',[App\Http\Controllers\CourseController::class,'show'])->name('course.show');
 Route::post('course',[App\Http\Controllers\CourseController::class,'store'])->name('course.save');
 Route::put('course/{course}',[App\Http\Controllers\CourseController::class,'update'])->name('course.update');
 Route::delete('course/{course}',[App\Http\Controllers\CourseController::class,'destory'])->name('course.destroy');
+//--------------Qualification---------------------------
+Route::get('qualification',[App\Http\Controllers\QualificationController::class,'index'])->name('qualification.list');
+Route::get('qualification/{qualification}',[App\Http\Controllers\QualificationController::class,'show'])->name('qualification.show');
+Route::post('qualification',[App\Http\Controllers\QualificationController::class,'store'])->name('qualification.save');
+Route::put('qualification/{qualification}',[App\Http\Controllers\QualificationController::class,'update'])->name('qualification.update');
+Route::delete('qualification/{qualification}',[App\Http\Controllers\QualificationController::class,'destory'])->name('qualification.destroy');
